9ebe30dc21aaaa1bb172330a442645f77fe64e13
Deb Johnson <friendswithdrj@gmail.com>
2020-06-18 15:06:06 -0500

    Change db test to use test helper functions

--- src/db.test.js
+++ src/db.test.js
@@ -1,18 +1,10 @@
-import { MongoClient } from 'mongodb';
 import { expect } from 'chai';
 import { getUserByUsername } from './db';
+import { getDatabaseData, setDatabaseData, resetDatabase} from './test-helpers';
 
 describe('getUserByUsername', () => {
   it('gets the correct user from the database given a username', async () => {
-    const client = await MongoClient.connect(
-      `mongodb://localhost:27017/TEST_DB`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    );
-    const db = client.db('TEST_DB');
-    //tests
+
     const fakeData = [{
       id: '123',
       username: 'abc',
@@ -23,11 +15,10 @@ describe('getUserByUsername', () => {
         email: 'wrong@wrong.com',
     }];
 
-    await db.collection('users').insertMany(fakeData);
+    await setDatabaseData('users', fakeData);
     const actual = await getUserByUsername('abc');
-    const finalDBState = await db.collection('users').find().toArray();
-    await db.dropDatabase();
-    client.close();
+    const finalDBState = await getDatabaseData('users');
+    await resetDatabase();
 
     const expected = {
       id: '123',
